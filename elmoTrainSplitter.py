4e406d5c78cfd8c035fc21b707120bec69b9d79e
trpelonski <trpovski@gmail.com>
2019-04-09 16:58:57 +0200

    Mala modifikacija

--- elmoTrainSplitter.py
+++ elmoTrainSplitter.py
@@ -9,22 +9,23 @@ def startProgram():
     root = tk.Tk()
     root.withdraw()
     filepath = filedialog.askopenfilename()
+    directoryName = input("Kako zelite da se zove direktorijum u koji ce da se smeste fajle za treniranje: ")
     fullFile = input("Da li zelite da isparsirate ceo fajl (Y/N): ")
     if fullFile == "Y" or fullFile == "y":
-        createTrainFiles(filepath,0,-1)
+        createTrainFiles(filepath,directoryName,0,-1)
     elif fullFile == "N" or fullFile == "n":
         splitStartStr = input("Unesite od koje recenzije zelite da zapocnete generisanje fajli za treniranje: ")
         splitLenStr = input("Unesite koliko recenzija zelite da bude iskorisceno za treniranje: ")
         try:
             splitStart = int(splitStartStr)
             splitLen = int(splitLenStr)
-            createTrainFiles(filepath,splitStart,splitLen)
+            createTrainFiles(filepath,directoryName,splitStart,splitLen)
         except ValueError:
             print("Unos neuspesan!")
 
-def createTrainFiles(filepath,splitStart,splitLen):
+def createTrainFiles(filepath,directoryName,splitStart,splitLen):
 
-    trainFilesDirectory = os.path.join(os.path.dirname(filepath),"trainParts")
+    trainFilesDirectory = os.path.join(os.path.dirname(filepath),directoryName)
     if not os.path.exists(trainFilesDirectory):
         os.mkdir(trainFilesDirectory)
 
@@ -34,17 +35,17 @@ def createTrainFiles(filepath,splitStart,splitLen):
         shuffle(sentences)
         fileNum = 1
         i = 0
-        for sentence in sentences:
+        for sentencesLen, sentence in enumerate(sentences):
             if i == 0:
                 trainFilePart = open(os.path.join(trainFilesDirectory,os.path.basename(filepath) + str(fileNum) + ".txt"),"w", encoding="utf-8")
                     
             trainFilePart.write(sentence)
-            if i != FILE_LEN-1:
+            if i != FILE_LEN-1 and sentencesLen != len(sentences)-1:
                 trainFilePart.write("\n")
 
             i += 1
 
-            if i == FILE_LEN:
+            if i == FILE_LEN or sentencesLen == len(sentences)-1:
                 fileNum += 1
                 i = 0
                 trainFilePart.close()
