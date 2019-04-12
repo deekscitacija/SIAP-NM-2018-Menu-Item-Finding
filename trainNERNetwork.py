51ce29d409450b09d504da6c6fc034afc7ace747
trpelonski <trpovski@gmail.com>
2019-04-12 13:59:02 +0200

    Odradjen padding tagova

--- trainNERNetwork.py
+++ trainNERNetwork.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from readConllevalFile import readConllevalFile
+from keras.preprocessing.sequence import pad_sequences
 
 def startProgram():
     root = tk.Tk()
@@ -9,20 +10,25 @@ def startProgram():
     with open(filepath,"r",encoding="utf-8") as conllevalFile:
         sentences = readConllevalFile(conllevalFile, '\t', [0,1,9])
         sentences_words = []
+        sentences_tags = []
         tags = set()
         
         for sentence in sentences:
                 sentence_words = []
+                sentence_tags = []
                 for sentenceElement in sentence:
                         sentence_words.append(sentenceElement[1])
-                        tags.add(sentenceElement[-1])
+                        sentence_tag = sentenceElement[-1]
+                        sentence_tags.append(sentence_tag)
+                        tags.add(sentence_tag)
                 sentences_words.append(sentence_words)
+                sentences_tags.append(sentence_tags)
 
         max_len = 200
+        tag2idx = {t: i for i, t in enumerate(list(tags))}
         sentence_words_padded = padSentenceWords(sentences_words, max_len)
-        print(sentence_words_padded[1])
-
-        tag2idx = {t: i for i, t in enumerate(tags)}
+        sentence_tags_converted = [[tag2idx[sentence_tag] for sentence_tag in sentence_tags] for sentence_tags in sentences_tags]
+        sentence_tags_padded = pad_sequences(maxlen=max_len, sequences=sentence_tags_converted, padding="post", value=tag2idx["O"])
 
 def padSentenceWords(sentences_words, max_len):
         sentences_words_padded = []
